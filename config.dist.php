21fba621c40eeb156e3ff95f219dda79279be4d8
Antoine Giraud <antoine.giraud@outlook.com>
2016-01-03 01:43:37 +0100

    MAJ config: on appelle PayIcam &donc on utilise le client json pour le contacter

--- config.dist.php
+++ config.dist.php
@@ -3,5 +3,7 @@
 // Chemin vers le serveur CAS (avec le / final)
 $_CONFIG['cas_url'] = "";
 
+$_CONFIG['payutc_server']  = "https://xxx/server/web";
+
 // URL publique http(s) vers admin_ginger (avec le / final)
-$_CONFIG['accueil-payicam'] = "http://xxx/accueil-payicam/";
+$_CONFIG['accueil-payicam'] = "https://xxx/accueil-payicam/";
