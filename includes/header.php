a834d4a51c932da7fe282755228ffacd09f8a77e
Antoine Giraud <antoine.giraud@outlook.com>
2016-01-05 10:22:21 +0100

    tentative pour faire marcher le responsive

--- includes/header.php
+++ includes/header.php
@@ -2,10 +2,22 @@
 <html lang="fr">
   <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
      <!-- Le styles -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/main.css" rel="stylesheet">
     <link rel="shortcut icon" href="favicon.png" />
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="http://getbootstrap.com/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
     <title><?php if(isset($title_for_layout)){echo $title_for_layout.' - ';} ?><?= WEBSITE_TITLE; ?></title>
 
--- index.php
+++ index.php
@@ -11,7 +11,7 @@
   <p>Ceci est la page d'accueil de payicam, vous pouvez choisir l'application que vous souhaitez utiliser </p>
 </div>
 <div class="row">
-  <div class="col-sm-6">
+  <div class="col-md-6">
     <div class="row">
       <div class="col-sm-8">
         <h3>Rechargement</h3>
@@ -21,7 +21,7 @@
       <div class="col-sm-4"><img class="img-responsive" style="margin-top: 30px;max-height: 150px;" src="img/gold_card.png"/></p></div>
     </div>
   </div>
-  <div class="col-sm-6">
+  <div class="col-md-6">
     <div class="row">
       <div class="col-sm-8">
         <h3>Billeterie</h3>
@@ -31,7 +31,7 @@
       <div class="col-sm-4"><img class="img-responsive" style="margin-top: 30px;max-height: 150px;" src="img/tickets.png"/></p></p></div>
     </div>
   </div>
-  <div class="col-sm-6">
+  <div class="col-md-6">
     <div class="row">
       <div class="col-sm-8">
         <h3>Associations</h3>
@@ -41,7 +41,7 @@
       <div class="col-sm-4"><img class="img-responsive" style="margin-top: 30px;max-height: 150px;" src="img/wave.png"/></p></div>
     </div>
   </div>
-  <div class="col-sm-6">
+  <div class="col-md-6">
     <div class="row">
       <div class="col-sm-8">
         <h3>Les évènements</h3>
@@ -56,24 +56,24 @@
 <?php if ($Auth->hasRole('admin')): ?>
   <h2 class="page-header">Liens vers l'Administration</h2>
   <div class="row">
-    <div class="col-sm-4">
+    <div class="col-md-4">
       <h3>Admin PayIcam</h3>
       <p>Application web permettant entre autre la gestion des articles, la gestion des droits, la trésorerie, ...</p>
       <p><a class="btn btn-primary" href="../scoobydoo" target="blank" role="button">Scoobydoo &raquo;</a>
     </div>
     <?php if ($Auth->hasRole('super-admin')): ?>
-    <div class="col-sm-4">
+    <div class="col-md-4">
       <h3>Gestion des données des élèves</h3>
       <p>Cette interface permet la gestion par exemple de l'affectation des identifiants cartes étudiantes aux élèves.</p>
       <p><a class="btn btn-primary" href="../admin_ginger" target="blank" role="button">Admin Ginger &raquo;</a>
     </div>
     <?php endif ?>
-    <div class="col-sm-4">
+    <div class="col-md-4">
       <h3>Vente par caisse physique</h3>
       <p>Application web de ventre des articles comme au Bar ou la cafet avec une caisse et une badgeuse.</p>
       <p><a class="btn btn-primary" href="../mozart" target="blank" role="button">Mozart &raquo;</a>
     </div>
-    <div class="col-sm-4">
+    <div class="col-md-4">
       <h3>Admin ventes en ligne</h3>
       <p>Administration des ventes d'articles en ligne, celle de shotgun.</p>
       <p><a class="btn btn-primary" href="../shotgun/admin" target="blank" role="button">Shotgun &raquo;</a>
