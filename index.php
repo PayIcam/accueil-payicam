01ca6ec45b98d23f4fbdc9979057a82a3cfbc5e3
Antoine Giraud <antoine.giraud@outlook.com>
2016-01-17 23:51:31 +0100

    meilleure sémentique pour les boutons + titre

--- index.php
+++ index.php
@@ -9,25 +9,13 @@
 <div class="jumbotron">
   <h1>Bienvenue sur PayIcam !</h1>
   <p>Ceci est la page d'accueil de payicam, vous pouvez choisir l'application que vous souhaitez utiliser </p>
-  <button type="button" class="btn btn-default" aria-label="Left Align">
-    <span class="glyphicon glyphicon-envelope"></span>
-    <a href="https://webmail.icam.fr/webmail/" target="_blank">Mail Icam</a>
-  </button>
-  <button type="button" class="btn btn-default" aria-label="Left Align">
-    <span class="glyphicon glyphicon-calendar"></span>
-    <a href="http://icam-ecap.capvalley.fr/" target="_blank">Planning Ecap Valley</a>
-  </button>
-  <button type="button" class="btn btn-default" aria-label="Left Align">
-    <span class="glyphicon glyphicon-lock"></span>
-    <a href="https://mail2.icam.fr/password/" target="_blank">Mot de passe ICAM</a>
-  </button>
-  <button type="button" class="btn btn-default" aria-label="Left Align">
-    <span class="glyphicon glyphicon-file"></span>
-    <a href="https://portfolio.icam.fr/" target="_blank">Portfolio Icam</a>
-  <button type="button" class="btn btn-default" aria-label="Left Align">
-    <span class="glyphicon glyphicon-folder-open"></span>
-    <a href="https://moodle.icam.fr/" target="_blank">Moodle Icam</a>
-  </button>
+  <p>
+    <a class="btn btn-default" href="https://webmail.icam.fr/webmail/" target="_blank"><span class="glyphicon glyphicon-envelope"></span> Mail Icam</a>
+    <a class="btn btn-default" href="http://icam-ecap.capvalley.fr/" target="_blank" title="Lien vers l'agenda Ecap Valley <?= "\n" ?>Pour se connecter, mettons pour antoine.giraud@2015.icam.fr,<?= "\n" ?>il faut metre Agiraud et en mdp ici115"><span class="glyphicon glyphicon-calendar"></span> Agenda Ecap Valley</a>
+    <a class="btn btn-default" href="https://mail2.icam.fr/password/" target="_blank" title="C'est important de changer son mot de passe,<?= "\n" ?>surtout si vous avez des resposabilités sur des associations de PayIcam...<?= "\n" ?>Si jamais vous n'avez pas à vous inscrire pour la première fois à PayIcam,<?= "\n" ?>Tentez de mettre un mdp sans % ou #"><span class="glyphicon glyphicon-lock"></span> Mot de passe ICAM</a>
+    <a class="btn btn-default" href="https://portfolio.icam.fr/" target="_blank"><span class="glyphicon glyphicon-file"></span> Portfolio Icam</a>
+    <a class="btn btn-default" href="https://moodle.icam.fr/" target="_blank"><span class="glyphicon glyphicon-folder-open"></span> Moodle Icam</a>
+  </p>
 </div>
 <div class="row">
   <div class="col-md-6">
