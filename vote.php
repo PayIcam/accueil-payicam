dbb82ffb9f949db695433842b876b1fa344101d4
Guillaume Dubois <dbmgui@gmail.com>
2017-11-21 08:23:28 +0100

    Essai bootstrap 4 sans header

--- vote.php
+++ vote.php
@@ -30,10 +30,22 @@
 
 
 	$title_for_layout = 'Election BDE';
-	include 'includes/header.php'; // insertion du fichier header.php : entête, barre de navigation
-
 ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Vote BDE</title>
+		<meta name="description" content="Site de vote pour la campagne BDE de l'Icam de Lille">
+		<link href="bootstrap_v4/css/bootstrap.css" rel="stylesheet">
+		<link href="css/main.css" rel="stylesheet">
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
 
 	<div class ="container" id="morph">
 		<!-- <img src="morpheus.jpeg" alt="morpheus" style="width:100%;"> -->
