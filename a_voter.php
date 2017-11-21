b9306a4982afed1a0606027e3f8c41094a92b4b8
Guillaume Dubois <dbmgui@gmail.com>
2017-11-21 19:16:51 +0100

    un drole de petit garçon

--- a_voter.php
+++ a_voter.php
@@ -46,11 +46,11 @@ catch(Exeption $e)
     // }
 
     $user = $Auth->getUser();
-	var_dump($user);
+	// var_dump($user);
 
 	$vote = $DB->query('SELECT * FROM vote WHERE slug = "elections-bde-2017"');
 	$my_vote = $DB->query('SELECT * FROM vote_has_voters WHERE email = :email', ['email' => $user['email']]);
-	$promo = $DB_promo->query('SELECT promo FROM users WHERE email = :email', ['email' => $user['email']]);
+	// $promo = $DB_promo->query('SELECT promo FROM users WHERE email = :email', ['email' => $user['email']]);
 	// var_dump($vote);
 	// var_dump($my_vote);
 	if (count($my_vote)>0){
@@ -59,12 +59,13 @@ catch(Exeption $e)
 	}
 
 	$enreg= $DB->prepare('INSERT INTO vote_has_voters (vote_id, email, date_vote, promo, choice) values (:vote_id, :email, :date_vote, :promo, :choice)');
-	
+
 	$date_vote=date('Y-m-d H:i:s');
 	$enreg -> bindParam('vote_id', 1, PDO::PARAM_INT);
 	$enreg -> bindParam('email', $_SESSION['Auth']['email'], PDO::PARAM_STR);
 	$enreg -> bindParam('date_vote', $date_vote, PDO::PARAM_STR);
-	$enreg -> bindParam('promo', $promo, PDO::PARAM_INT);
+	// $enreg -> bindParam('promo', $promo, PDO::PARAM_INT);
+	$enreg -> bindParam('promo', 119, PDO::PARAM_INT);
 	$enreg -> bindParam('choice', $_POST, PDO::PARAM_STR);
 	try {
 		$enreg -> execute();
