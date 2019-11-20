918fcb8fdbec1507dbcba39afe3766de30e820d9
GregoireGiraud <gregoiregiraud1@gmail.com>
2019-11-20 13:43:07 +0100

    fix

--- vote.php
+++ vote.php
@@ -80,8 +80,8 @@ include 'includes/header.php';
     <!-- <div class="sep_bouton" id="sep_bouton_choix_3">
       <img src="img/choix1.png" type="button" alt="choix_3" class='rounded-circle' id="choix_3">
     </div> -->
-    <div id="sep_bouton_bas">
-        <img src="img/choix.png" type="button" alt="choix_1" class='rounded-circle' id="choix_1" data-toggle="modal" data-target="#blancModal">
+    <div class="sep_bouton" id="sep_bouton_bas">
+        <img src="img/choix2.jpg" type="button" alt="choix_2" class='rounded-circle' id="choix_1" data-toggle="modal" data-target="#blancModal">
       <input type="button" class="btn btn-secondary btn-lg" value="Je vote blanc" data-toggle="modal" data-target="#blancModal"></input>
     </div>
   </div>
