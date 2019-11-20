c3c1837dfcb9b19a287da2cda8b5a07218d996aa
GregoireGiraud <gregoiregiraud1@gmail.com>
2019-11-20 14:08:18 +0100

    ne jamais parler trop vite

--- vote.php
+++ vote.php
@@ -81,7 +81,7 @@ include 'includes/header.php';
       <img src="img/choix1.png" type="button" alt="choix_3" class='rounded-circle' id="choix_3">
     </div> -->
     <div class="sep_bouton" id="sep_bouton_choix_2">
-        <img src="img/choix2.jpg" type="button" alt="choix_2" class='rounded-circle' id="choix_2" data-toggle="modal" data-target="#blancModal">
+        <img src="img/choix2.png" type="button" alt="choix_2" class='rounded-circle' id="choix_2" data-toggle="modal" data-target="#blancModal">
       <!-- <input type="button" class="btn btn-secondary btn-lg" value="Je vote blanc" data-toggle="modal" data-target="#blancModal"></input> -->
     </div>
   </div>
