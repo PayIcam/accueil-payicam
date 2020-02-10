f5e925e5c88be4b2d774828337a3e1e3c263e80a
GregoireGiraud <gregoiregiraud1@gmail.com>
2020-02-10 15:26:05 +0100

    last db

--- modifier_reservation.php
+++ modifier_reservation.php
@@ -2,16 +2,10 @@
 
 require_once 'includes/_header.php';
 
-try {
-    $DB = new PDO('mysql:host=localhost;dbname=payicam_accueil;charset=utf8','root','',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION ));
-} catch(Exeption $e) {
-    die('erreur:'.$e->getMessage());
-}
-
-$requete_reservations = $DB->query("SELECT * FROM reservation");
+$requete_reservations = $accueil_payicam->query("SELECT * FROM reservation");
 $reservations = $requete_reservations->fetchAll();
 
-$update = $DB->prepare('UPDATE reservation SET status=:status WHERE reservation_id=:reservation_id');
+$update = $accueil_payicam->prepare('UPDATE reservation SET status=:status WHERE reservation_id=:reservation_id');
 $update->execute(array('status' => $_GET['reservation_status'], 'reservation_id' => $_GET['reservation_id']));
 
 header('Location: reservation_admin.php');
--- supprimer_objet.php
+++ supprimer_objet.php
@@ -2,13 +2,7 @@
 
 require_once 'includes/_header.php';
 
-try {
-    $DB = new PDO('mysql:host=localhost;dbname=payicam_accueil;charset=utf8','root','',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION ));
-} catch(Exeption $e) {
-    die('erreur:'.$e->getMessage());
-}
-
-$createur = $DB->prepare('SELECT email FROM item WHERE item_id=:item_id');
+$createur = $accueil_db->prepare('SELECT email FROM item WHERE item_id=:item_id');
 $createur->execute(array('item_id' => $_GET['object_id']));
 $createur = $createur->fetch()['email'];
 
@@ -16,7 +10,7 @@ if ($Auth->getUser()['email'] !== $createur) {
 	Functions::setFlashAndRedirect('Vous ne pouvez pas supprimer les objets des autres', 'danger', 'reservation.php');
 }
 
-$delete = $DB->prepare('UPDATE item SET visibility=:visibility WHERE item_id=:item_id');
+$delete = $accueil_db->prepare('UPDATE item SET visibility=:visibility WHERE item_id=:item_id');
 $delete->execute(array('visibility' => 1, 'item_id' => $_GET['object_id']));
 
 header ('location: reservation.php');
