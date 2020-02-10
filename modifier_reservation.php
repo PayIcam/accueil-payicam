fba2d3b9879583738ee6a8a89c437d3b45c8e826
GregoireGiraud <gregoiregiraud1@gmail.com>
2020-02-10 15:27:17 +0100

    fix

--- modifier_reservation.php
+++ modifier_reservation.php
@@ -2,10 +2,10 @@
 
 require_once 'includes/_header.php';
 
-$requete_reservations = $accueil_payicam->query("SELECT * FROM reservation");
+$requete_reservations = $accueil_db->query("SELECT * FROM reservation");
 $reservations = $requete_reservations->fetchAll();
 
-$update = $accueil_payicam->prepare('UPDATE reservation SET status=:status WHERE reservation_id=:reservation_id');
+$update = $accueil_db->prepare('UPDATE reservation SET status=:status WHERE reservation_id=:reservation_id');
 $update->execute(array('status' => $_GET['reservation_status'], 'reservation_id' => $_GET['reservation_id']));
 
 header('Location: reservation_admin.php');
