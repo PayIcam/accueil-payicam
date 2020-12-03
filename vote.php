a4e9133a8022b578f29edb4dd31e0e811a1e6e84
GregoireGiraud <gregoiregiraud1@gmail.com>
2020-12-03 15:28:16 +0100

    :)))))

--- vote.php
+++ vote.php
@@ -68,7 +68,7 @@ include 'includes/header.php';
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="choix_2_label">Voter Mirage</h5>
+                        <h5 class="modal-title" id="choix_2_label">Voter Bohemian</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
@@ -91,7 +91,7 @@ include 'includes/header.php';
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="choix_2_label">Voter Pyramide</h5>
+                        <h5 class="modal-title" id="choix_2_label">Voter Eldorado</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
